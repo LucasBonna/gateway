0585b3b77e5b92f46ae931553c03df8afc33b2e9
Lucas Bonna Favaro <88051985+LucasBonna@users.noreply.github.com>
2024-12-15 01:18:35 -0300

    fix: cors filter

--- src/main/java/br/com/contafacil/bonnarotec/gateway/filter/CorsFilter.java
+++ src/main/java/br/com/contafacil/bonnarotec/gateway/filter/CorsFilter.java
@@ -8,15 +8,22 @@ import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements WebFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(CorsFilter.class);
+
     @Value("${frontend_url}")
     private String frontendUrl;
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        logger.info("Configurando CORS para origin: {}", frontendUrl);
+
         exchange.getResponse().getHeaders().add("Access-Control-Allow-Origin", frontendUrl);
         exchange.getResponse().getHeaders().add("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
         exchange.getResponse().getHeaders().add("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization, Cookie");
@@ -24,8 +31,8 @@ public class CorsFilter implements WebFilter {
         exchange.getResponse().getHeaders().add("Access-Control-Max-Age", "3600");
         exchange.getResponse().getHeaders().add("Access-Control-Expose-Headers", "Authorization, Set-Cookie");
 
-        if ("OPTIONS".equals(exchange.getRequest().getMethod().name())) {
-            exchange.getResponse().getHeaders().add("Access-Control-Max-Age", "3600");
+        if ("OPTIONS".equalsIgnoreCase(exchange.getRequest().getMethod().name())) {
+            logger.info("Requisição de preflight (OPTIONS) detectada. Respondendo com 200 OK.");
             exchange.getResponse().setStatusCode(org.springframework.http.HttpStatus.OK);
             return Mono.empty();
         }
